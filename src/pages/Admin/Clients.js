425c7b79b99bb9e14b6b23e1950bd9a33664adfa
mariemhajjem <mariemhajjem10@gmail.com>
2022-11-16 16:58:27 +0100

    add user crud

--- src/pages/Admin/Clients.js
+++ src/pages/Admin/Clients.js
@@ -1,161 +1,158 @@
 import {
-    Row,
-    Col,
-    Card,
-    Radio,
-    Table,
-    Button,
-    Avatar,
-    Typography,
-    Divider,
-  } from "antd";
-import { useState } from "react";
-  
-  // Images
-  import face2 from "../../assets/images/face-2.jpg";
-  
-  const { Title } = Typography;
+  Row,
+  Col,
+  Card,
+  Radio,
+  Table,
+  Button,
+  Avatar,
+  Typography,
+  Divider,
+} from "antd";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-  // table code start
-  const columns = [
-    {
-      title: "CLIENT",
-      dataIndex: "name",
-      key: "name",
-      width: "32%",
-    },
-    {
-      title: "FUNCTION",
-      dataIndex: "function",
-      key: "function",
-    },
-  
-    {
-      title: "STATUS",
-      key: "status",
-      dataIndex: "status",
-    },
-    {
-      title: "Modifier",
-      key: "update",
-      dataIndex: "update",
-    },
-  ];
-  
-  const data = [
-    {
+// Images
+import face2 from "../../assets/images/face-2.jpg";
+import { getAllUsers } from "../../redux/reducers/users";
+
+const { Title } = Typography;
+
+// table code start
+const columns = [
+  {
+    title: "Utilisateur",
+    dataIndex: "name",
+    key: "name",
+    width: "32%",
+  },
+  {
+    title: "FUNCTION",
+    dataIndex: "function",
+    key: "function",
+  },
+
+  {
+    title: "Numéro de téléphone",
+    key: "phoneNumber",
+    dataIndex: "phoneNumber",
+  },
+  {
+    title: "Bloquer",
+    key: "block",
+    dataIndex: "block",
+  },
+];
+
+
+
+function Clients() { 
+  const [err, setError] = useState("");
+  const [code, setCode] = useState("");
+  const { list } = useSelector((state) => state.users);
+  // const userId = token?.user?._id;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    /* if (userId) {
+      dispatch(getToursByUser(userId));
+    } */
+    dispatch(getAllUsers())
+  }, []);
+  /* const error = useSelector((state) => state.errorReducer);
+   
+  const popUp = (type) => {
+    notification[type]({
+      message: code,
+      description: err,
+      onClose: clearError,
+    });
+  }; */ 
+  const onChange = (e) => console.log(`radio checked:${e.target.value}`);
+
+  const data = list?.map((user) => ({
       key: "1",
       name: (
         <>
           <Avatar.Group>
-            <Avatar
+            {/* <Avatar
               className="shape-avatar"
               shape="square"
               size={40}
               src={face2}
-            ></Avatar>
+            ></Avatar> */}
             <div className="avatar-info">
-              <Title level={5}>Michael John</Title>
-              <p>michael@mail.com</p>
+              <Title level={5}>{`${user?.firstName} ${user?.lastName}`}</Title>
+              <p>{user?.email}</p>
             </div>
-          </Avatar.Group>{" "}
+          </Avatar.Group>
         </>
       ),
       function: (
         <>
           <div className="author-info">
-            <Title level={5}>Manager</Title>
-            <p>Organization</p>
+            <Title level={5}>{user?.user_grade} </Title>
+            <p></p>
           </div>
         </>
       ),
-     status: (
+      phoneNumber: (
+        <div className="author-info">
+            <Title level={5}>{user?.phoneNumber} </Title>
+            <p></p>
+        </div>
+      ),
+      block: (
         <>
-          <Button type="primary" className="tag-primary">
-            ONLINE
-          </Button>
-        </>
-      ), 
-      update: (
-        <>
-          <Button type="primary" className="tag-primary">
-            modifier
+          <Button type="primary" danger className="tag-primary">
+            bloquer
           </Button>
         </>
       ),
-    }
-  ];
-  
-  function Clients() {
-    const [isAddVisible, setIsAddVisible] = useState(false);
-    const [isUpdateVisible, setIsUpdateVisible] = useState(false);
-    const [err, setError] = useState("");
-    const [code, setCode] = useState("");
-    /* const error = useSelector((state) => state.errorReducer);
-    useEffect(() => {
-      if (error.message) {
-        setError(error.message);
-        setCode(error.code);
-      }
-    }, [error]);
-    const popUp = (type) => {
-      notification[type]({
-        message: code,
-        description: err,
-        onClose: clearError,
-      });
-    }; */
-    const handleClick = event => {
-      // 👇️ toggle visibility
-      setIsAddVisible(current => !current);
-    };
-    const onChange = (e) => console.log(`radio checked:${e.target.value}`);
-  
-    return (
-      <>
-        <div className="tabled"> 
-          <Row gutter={[24, 0]}>
+    }));
+  return (
+    <>
+      <div className="tabled">
+        <Row gutter={[24, 0]}>
           <Col xs="24" xl={24}>
-        <Button
+            {/*<Button
               type="primary"
               onClick={() => setIsAddVisible(true)}
             >
               Ajouter client
             </Button>
-            {/* {err && popUp("error")}
+             {err && popUp("error")}
             {isAddVisible && <AddClient />}
             {isUpdateVisible && <UpdateClient />} */}
-            </Col>
+          </Col>
           <Divider orientation="left"></Divider>
-            <Col xs="24" xl={24}>
-              <Card
-                bordered={false}
-                className="criclebox tablespace mb-24"
-                title="Clients Table"
-                extra={
-                  <>
-                    <Radio.Group onChange={onChange} defaultValue="a">
-                      <Radio.Button value="a">All</Radio.Button>
-                      <Radio.Button value="b">ONLINE</Radio.Button>
-                    </Radio.Group>
-                  </>
-                }
-              >
-                <div className="table-responsive">
-                  <Table
-                    columns={columns}
-                    dataSource={data}
-                    pagination={false}
-                    className="ant-border-space"
-                  />
-                </div>
-              </Card>
-            </Col>
-          </Row>
-        </div>
-      </>
-    );
-  }
-  
-  export default Clients;
-  
+          <Col xs="24" xl={24}>
+            <Card
+              bordered={false}
+              className="criclebox tablespace mb-24"
+              title="Clients Table"
+              extra={
+                <>
+                  <Radio.Group onChange={onChange} defaultValue="a">
+                    <Radio.Button value="a">All</Radio.Button>
+                    <Radio.Button value="b">ONLINE</Radio.Button>
+                  </Radio.Group>
+                </>
+              }
+            >
+              <div className="table-responsive">
+                <Table
+                  columns={columns}
+                  dataSource={data}
+                  pagination={false}
+                  className="ant-border-space"
+                />
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
+
+export default Clients;
--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,55 +1,28 @@
 import Axios from "axios";
 
-let baseUrl = "http://localhost:5000/api/employees";
+let baseUrl = "http://localhost:5000/api/users";
  
 export const getAllUsers = async () => {
-  return await Axios.get(baseUrl + "/");
-  try {
-    const result = await Axios.get(baseUrl + "/");
-    return result.data;
-  } catch (error) {
-    console.log(error.response?.data)
-  }
+  return await Axios.get(baseUrl + "/"); 
 }
 
 
 export const createNewUser = async (user) => {
-  return await Axios.post(baseUrl + "/", user);
-  try {
-    const result = await Axios.post(baseUrl + "/", user);
-    return result.data;
-  } catch (error) {
-    console.log(error.response?.data)
-  }
-
+  return await Axios.post(baseUrl + "/", user); 
 }
 
 export const updateUser = async (user) => {
-  return await Axios.put(baseUrl + "/", user);
-  try {
-    const result = await Axios.put(baseUrl + "/", user);
-    return result.data;
-  } catch (error) {
-    console.log(error.response?.data)
-  }
+  return await Axios.put(baseUrl + "/", user); 
 }
 
 export const getUser = async (idUser) => {
-  return await Axios.get(baseUrl + "/" + idUser);
-  try {
-    const result = await Axios.get(baseUrl + "/" + idUser);
-    return result.data;
-  } catch (error) {
-    console.log(error.response?.data)
-  }
+  return await Axios.get(baseUrl + "/" + idUser); 
 }
 
 export const deleteUser = async (idUser) => {
-  return await Axios.delete(baseUrl + "/" + idUser);
-  try {
-    const result = await Axios.delete(baseUrl + "/" + idUser);
-    return result.data;
-  } catch (error) {
-    console.log(error.response?.data)
-  }
+  return await Axios.delete(baseUrl + "/" + idUser); 
+}
+
+export const blockUser = async (idUser) => {
+  return await Axios.put(baseUrl + "/block" + idUser); 
 }
